8362998842865307f7947d64685efe2e3915fae8
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-09-28 11:05:16 -0500

    about page updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,10 +7,7 @@ export default function About() {
     <div className="background">
       <h1 className='aboutTitle'>Justin Reese</h1>
       <p className ='aboutText'>
-            Student at UT Austin for Full Stack web developer. Currently studying the MERN Stack (MongoDB,
-            Express, React, and Nodejs) . Strong ability to create dynamic code and maintain strong
-            communication within my collaborators on projects. Worked fast pace while in my coding boot camp
-            with others of diverse experience and still maintained the ability to exceed expectations
+      Full Stack web developer leveraging business management background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of Texas at Austin.
           </p>
     </div>
   );
