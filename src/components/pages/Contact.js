2b953e8ffe32c365a1f64de15f19d575d4eb7202
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-10-22 12:15:23 -0500

    update resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
         <li className='contactButton'><Button href="mailto:justinreese160@gmail.com" variant="dark" size="lg" >Email</Button></li>
         <li className='contactButton'><Button href="https://github.com/justinreese160" variant="dark" size="lg">GitHub</Button></li>
         <li className='contactButton'> <Button href="https://www.linkedin.com/in/justin-reese-849510213" variant="dark" size="lg">LinkedIn</Button></li>
-        <li className='contactButton'><Button href="https://docs.google.com/document/d/1SPJODQuQqDMKv5yzk3Ak0ikHFNuF2fYHwAgUcpGRdr4/edit?usp=sharing" variant="dark" size="lg">Resume</Button></li>
+        <li className='contactButton'><Button href="https://docs.google.com/document/d/1thdfRXJHbZm469PoNUGJV5aaSnLmpt3E/edit?usp=sharing&ouid=118167267838735308249&rtpof=true&sd=true" variant="dark" size="lg">Resume</Button></li>
     </ul>
 </div>
 );
